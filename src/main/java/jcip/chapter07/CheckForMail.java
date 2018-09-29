da1be5308bf87dbf39110370a2f7d6d483583bbc
mawenlong <15061886113@163.com>
2018-09-29 23:40:31 +0800

    Java并发编程实战 第七章 取消与关闭

---
+++ src/main/java/jcip/chapter07/CheckForMail.java
@@ -0,0 +1,41 @@
+package jcip.chapter07;
+
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author mawenlong
+ * @date 2018/09/29
+ */
+public class CheckForMail {
+
+  public boolean checkMail(Set<String> hosts, long timeout, TimeUnit unit)
+      throws InterruptedException {
+    ExecutorService exec = Executors.newCachedThreadPool();
+    final AtomicBoolean hasNewMail = new AtomicBoolean(false);
+    try {
+      for (final String host : hosts) {
+        exec.execute(new Runnable() {
+          @Override
+          public void run() {
+            if (checkMail(host)) {
+              hasNewMail.set(true);
+            }
+          }
+        });
+      }
+    } finally {
+      exec.shutdown();
+      exec.awaitTermination(timeout, unit);
+    }
+    return hasNewMail.get();
+  }
+
+  private boolean checkMail(String host) {
+//    check for  mail
+    return false;
+  }
+}
